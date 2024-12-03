176c59d3715bd03f6665d4cadc81feee60642d4f
itsOwlbit <51840045+itsOwlbit@users.noreply.github.com>
2024-12-03 10:19:49 -0800

    Get input from file

--- day-3/Program.cs
+++ day-3/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Test file: day-3/aoc-day3-test.txt
+// Data file: day-3/aoc-day3-data.txt
+DayThreeAoC dayThreeAoc = new DayThreeAoC("day-3/aoc-day3-test.txt");
+
+// to check what was stored from file
+Console.WriteLine(dayThreeAoc.Data);
+
+class DayThreeAoC
+{
+    private readonly string pattern = @"mul\(\d{1,3},\d{1,3}\)";
+    private readonly string data;
+    public string Data => (data);
+    private List<string> dataParts;
+
+    public DayThreeAoC(string filePath)
+    {
+        data = ReadDataFromFile(filePath);
+    }
+
+    private string ReadDataFromFile(string filePath)
+    {
+        string dataFromFile = "";
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                dataFromFile = sr.ReadToEnd();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+
+        return dataFromFile;
+    }
+
+}
