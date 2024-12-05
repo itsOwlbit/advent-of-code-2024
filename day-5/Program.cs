70eb1f4c87cc6a98ea4e38686305846b8956e67f
itsOwlbit <51840045+itsOwlbit@users.noreply.github.com>
2024-12-05 12:22:13 -0800

    Display correctly ordered prints

--- day-5/Program.cs
+++ day-5/Program.cs
@@ -1,12 +1,14 @@
 ﻿string filePath = "day-5/aoc-day5-test.txt";
 
-Printer safetyProtocolPrintJob = new Printer(filePath);
-safetyProtocolPrintJob.PrintData();
+Printer safetyProtocolPrintQueue = new Printer(filePath);
+// safetyProtocolPrintQueue.PrintData();
+safetyProtocolPrintQueue.PrintQueue();
 
 class Printer
 {
-    private readonly List<(int pageNumber, int precedingPageNumber)> _pageOrderRules;
+    private readonly List<(int pageNumber, int precedesPageNumber)> _pageOrderRules;
     private readonly List<List<int>> _printQueue;
+    private int _sumOfMiddlePageValue = 0;
 
     public Printer(string filePath)
     {
@@ -30,9 +32,9 @@ class Printer
 
                     if (parts.Length == 2 &&
                         int.TryParse(parts[0], out int pageNumber) &&
-                        int.TryParse(parts[1], out int precedingPageNumber))
+                        int.TryParse(parts[1], out int precedesPageNumber))
                     {
-                        _pageOrderRules.Add((pageNumber, precedingPageNumber));
+                        _pageOrderRules.Add((pageNumber, precedesPageNumber));
                         // Console.WriteLine($"Rule Added: ({pageNumber}, {precedingPageNumber})");
                     }
                 }
@@ -61,7 +63,7 @@ class Printer
         Console.WriteLine("\nPage Order Rules:");
         foreach (var rule in _pageOrderRules)
         {
-            Console.WriteLine($"{rule.pageNumber} | {rule.precedingPageNumber}");
+            Console.WriteLine($"{rule.pageNumber} | {rule.precedesPageNumber}");
         }
 
         Console.WriteLine("\nPrint Queue:");
@@ -69,5 +71,53 @@ class Printer
         {
             Console.WriteLine($"{string.Join(", ", queue)}");
         }
+    }
+
+    public void PrintQueue()
+    {
+        foreach (List<int> queue in _printQueue)
+        {
+            List<int> pages = new List<int>();
+            bool hasViolatesRule = false;
+
+            foreach (var page in queue)
+            {
+                if (isQueueInCorrectRuleOrder(pages, page))
+                {
+                    pages.Add(page);
+                }
+                else
+                {
+                    // Console.WriteLine($"Page order rule error: {page}");
+                    hasViolatesRule = true;
+                }
+            }
+
+            if (!hasViolatesRule)
+            {
+                // calculate the sum of the middle page number for each queue line
+                PrintPages(pages);
+            }
+        }
+
+        Console.WriteLine($"\nSum of middle page numbers (from correctly-ordered queues): {_sumOfMiddlePageValue}");
+    }
+
+    public bool isQueueInCorrectRuleOrder(List<int> currentQueue, int pageNumber)
+    {
+        foreach (var (page, precedes) in _pageOrderRules)
+        {
+            if (pageNumber == page && currentQueue.Contains(precedes))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void PrintPages(List<int> pages)
+    {
+        Console.WriteLine($"{string.Join(", ", pages)}");
     }
 }
