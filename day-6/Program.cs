08c90baa36dbe51b994ea2387834df05c1e8f2b4
itsOwlbit <51840045+itsOwlbit@users.noreply.github.com>
2024-12-06 15:50:48 -0800

    Solved part 1 puzzle

--- day-6/Program.cs
+++ day-6/Program.cs
@@ -1,12 +1,15 @@
-﻿string filePath = "day-6/aoc-day6-test.txt";
+﻿// string filePath = "day-6/aoc-day6-test.txt";
+string filePath = "day-6/aoc-day6-data.txt";
 
 LabMap myMap = new LabMap(filePath);
 
-myMap.PrintMap();
-myMap.PrintGuardPosition();
-myMap.PrintObstaclePositions();
+// myMap.PrintMap();
+// myMap.PrintGuardPosition();
+// myMap.PrintObstaclePositions();
 Console.WriteLine();
 myMap.MoveGuard();
+int xCount = myMap.CountXInMap();
+Console.WriteLine($"Number of 'X' in map: {xCount}");
 
 class LabMap
 {
@@ -86,42 +89,164 @@ class LabMap
         foreach (var position in _guardsPositions)
         {
             char direction = _map[position];
+            bool hasExitedMap = false;
+            int nextPosition = position;
 
-            switch (direction)
+            _map[position] = 'X';
+
+            while (!hasExitedMap)
             {
-                case var _ when direction == positionalChars[0]:
-                    GuardFacingUp();
-                    break;
-                case var _ when direction == positionalChars[1]:
-                    GuardFacingRight();
-                    break;
-                case var _ when direction == positionalChars[2]:
-                    GuardFacingDown();
-                    break;
-                case var _ when direction == positionalChars[3]:
-                    GuardFacingLeft();
-                    break;
-                default:
-                    Console.WriteLine($"Invalid direction at: {position}: {direction}");
-                    break;
+                switch (direction)
+                {
+                    case var _ when direction == positionalChars[0]:
+                        nextPosition = GuardFacingUp(nextPosition);
+                        break;
+                    case var _ when direction == positionalChars[1]:
+                        nextPosition = GuardFacingRight(nextPosition);
+                        break;
+                    case var _ when direction == positionalChars[2]:
+                        nextPosition = GuardFacingDown(nextPosition);
+                        break;
+                    case var _ when direction == positionalChars[3]:
+                        nextPosition = GuardFacingLeft(nextPosition);
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid direction at: {position}: {direction}");
+                        break;
+                }
+
+                // Change guard's position and facing direction
+                if (nextPosition >= 0 && nextPosition < _map.Count)
+                {
+                    ChangeGuardsPosition(position, nextPosition, direction);
+                    direction = _map[nextPosition];
+                }
+
+                if (nextPosition == -1)
+                {
+                    hasExitedMap = true;
+                    Console.WriteLine("Guard has exited map.");
+                }
+
+                // PrintMap();
             }
         }
     }
 
-    private void GuardFacingUp()
+    private int GuardFacingUp(int position)
     {
-        Console.WriteLine("Guard is facing up.");
+        // Console.WriteLine("Guard is facing up.");
+        _map[position] = 'X';
+
+        // Move up until obstacle
+        int nextStep = position - _cols;
+
+        while (nextStep >= 0)
+        {
+            if (_map[nextStep] == '#')
+            {
+                // Console.WriteLine($"Obstacle ahead at: {nextStep}");
+                return nextStep + _cols;
+            }
+
+            _map[nextStep] = 'X';
+            nextStep -= _cols;
+        }
+
+        Console.WriteLine($"Reached top of map from ({nextStep + _cols})");
+        return -1;
     }
-    private void GuardFacingRight()
+
+    private int GuardFacingRight(int position)
     {
-        Console.WriteLine("Guard is facing right.");
+        // Console.WriteLine("Guard is facing right.");
+        _map[position] = 'X';
+
+        // Move right until obstacle
+        int nextStep = position + 1;
+
+        while (nextStep % _cols != 0)
+        {
+            if (_map[nextStep] == '#')
+            {
+                // Console.WriteLine($"Obstacle ahead at: {nextStep}");
+                return nextStep - 1;
+            }
+
+            _map[nextStep] = 'X';
+            nextStep++;
+        }
+
+        Console.WriteLine($"Reached far right of map from ({nextStep - 1})");
+        return -1;
     }
-    private void GuardFacingDown()
+
+    private int GuardFacingDown(int position)
     {
-        Console.WriteLine("Guard is facing down.");
+        // Console.WriteLine("Guard is facing down.");
+        _map[position] = 'X';
+
+        // Move down until obstacle
+        int nextStep = position + _cols;
+
+        while (nextStep < _map.Count)
+        {
+            if (_map[nextStep] == '#')
+            {
+                // Console.WriteLine($"Obstacle ahead at: {nextStep}");
+                return nextStep - _cols;
+            }
+
+            _map[nextStep] = 'X';
+            nextStep += _cols;
+        }
+
+        Console.WriteLine($"Reached bottom of map from ({nextStep - _cols})");
+        return -1;
     }
-    private void GuardFacingLeft()
+
+    private int GuardFacingLeft(int position)
     {
-        Console.WriteLine("Guard is facing left.");
+        // Console.WriteLine("Guard is facing left.");
+        _map[position] = 'X';
+
+        // Move left until obstacle
+        int nextStep = position - 1;
+
+        while (nextStep >= 0 && nextStep % _cols != _cols - 1)
+        {
+            if (_map[nextStep] == '#')
+            {
+                // Console.WriteLine($"Obstacle ahead: {nextStep}");
+                return nextStep + 1;
+            }
+
+            _map[nextStep] = 'X';
+            nextStep--;
+        }
+
+        Console.WriteLine($"Reached far left of map from ({nextStep + 1})");
+        return -1;
+    }
+
+    private void ChangeGuardsPosition(int currentPosition, int newPosition, char currentDirection)
+    {
+        // Find the index of the current direction
+        int directionIndex = Array.IndexOf(positionalChars, currentDirection);
+        if (directionIndex == -1)
+        {
+            Console.WriteLine($"Invalid direction: {currentDirection}");
+            return;
+        }
+
+        // Update the new position with the correct character
+        _map[newPosition] = positionalChars[(directionIndex + 1) % positionalChars.Length];
+    }
+
+    public int CountXInMap()
+    {
+        int count = _map.Count(c => c == 'X');
+
+        return count;
     }
 }
