7df1d7640d67c06696eb5a1342c6fc397b3b0c34
itsOwlbit <51840045+itsOwlbit@users.noreply.github.com>
2024-12-10 09:20:27 -0800

    Display data from file

--- day-10/Program.cs
+++ day-10/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string filePath = "day-10/aoc-day10-test.txt";
+
+MyClass myClass = new MyClass(filePath);
+
+class MyClass
+{
+    private List<List<int>> _topographyMap;
+    private int _rows = 0;
+    private int _cols = 0;
+
+    public MyClass(string filePath)
+    {
+        _topographyMap = new List<List<int>>();
+
+        ReadDataFromFile(filePath);
+    }
+
+    private void ReadDataFromFile(string filePath)
+    {
+        using (StreamReader sr = new StreamReader(filePath))
+        {
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+
+                if (_cols == 0)
+                {
+                    _cols = line.Length;
+                }
+            }
+        }
+    }
+}
