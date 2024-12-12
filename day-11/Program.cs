0828c83d4e5df7f314b4adc25089e07bd7543385
itsOwlbit <51840045+itsOwlbit@users.noreply.github.com>
2024-12-11 19:35:48 -0800

    Solved with test data passing

--- day-11/Program.cs
+++ day-11/Program.cs
@@ -5,6 +5,13 @@ StonesLine stoneLine = new StonesLine(filepath);
 stoneLine.DisplayStones();
 Console.WriteLine();
 
+stoneLine.Blink(25);
+Console.WriteLine();
+
+int numberOfStones = stoneLine.GetNumberOfStones();
+Console.WriteLine($"Number of Stones: {numberOfStones}");
+Console.WriteLine();
+
 class StonesLine
 {
     List<int> _stones;
@@ -35,6 +42,49 @@ class StonesLine
                 }
             }
         }
+    }
+
+    public void Blink(int numberOfBlinks)
+    {
+        for (int i = 1; i <= numberOfBlinks; i++)
+        {
+            for (int j = 0; j < _stones.Count; j++)
+            {
+                // int length = _stones[i].ToString().Length;
+                // Console.WriteLine($"length: {length}");
+
+                if (_stones[j] == 0)
+                {
+                    _stones[j] = 1;
+                }
+                else if (_stones[j].ToString().Length % 2 == 0)
+                {
+                    int numberOfDigits = _stones[j].ToString().Length;
+                    int halfValue = numberOfDigits / 2;
+                    int leftStone = _stones[j] / (int)Math.Round(Math.Pow(10, halfValue));
+                    int rightStone = _stones[j] - (leftStone * (int)Math.Round(Math.Pow(10, halfValue)));
+
+                    // Console.WriteLine($"left stone: {leftStone}");
+                    // Console.WriteLine($"right stone: {rightStone}");
+
+                    _stones[j] = leftStone;
+                    _stones.Insert(j + 1, rightStone);
+                    j++;
+                }
+                else
+                {
+                    _stones[j] *= 2024;
+                }
+            }
+
+            Console.WriteLine($"After {i} blink(s):");
+            DisplayStones();
+        }
+    }
+
+    public int GetNumberOfStones()
+    {
+        return _stones.Count;
     }
 
     public void DisplayStones()
