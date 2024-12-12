78f9cda692b5ea21a5b630a3d0f3792bf2b08878
itsOwlbit <51840045+itsOwlbit@users.noreply.github.com>
2024-12-11 21:27:35 -0800

    Refactored to do all blinks per stone, but out of memory for part 2

--- day-11/Program.cs
+++ day-11/Program.cs
@@ -7,11 +7,15 @@ StonesLine stoneLine = new StonesLine(filepath);
 stoneLine.DisplayStones();
 Console.WriteLine();
 
-stoneLine.Blink(25);
-Console.WriteLine();
+// stoneLine.Blink(75);
+// Console.WriteLine();
 
-BigInteger numberOfStones = stoneLine.GetNumberOfStones();
-Console.WriteLine($"Number of Stones: {numberOfStones}");
+// BigInteger numberOfStones = stoneLine.GetNumberOfStones();
+// Console.WriteLine($"Number of Stones: {numberOfStones}");
+// Console.WriteLine();
+
+// part 2
+stoneLine.Blink2(75);
 Console.WriteLine();
 
 class StonesLine
@@ -80,6 +84,52 @@ class StonesLine
             Console.WriteLine($"Blink {i}: {_stones.Count}");
             Console.WriteLine("");
         }
+    }
+
+    public void Blink2(int numberOfBlinks)
+    {
+        BigInteger totalStones = 0;
+
+        foreach (var stone in _stones)
+        {
+            List<BigInteger> currentStones = new List<BigInteger> { stone };
+
+            Console.WriteLine($"Stone: {stone}");
+
+            for (int i = 1; i <= numberOfBlinks; i++)
+            {
+                List<BigInteger> newStones = new List<BigInteger>();
+
+                foreach (var currentStone in currentStones)
+                {
+                    if (currentStone == 0)
+                    {
+                        newStones.Add(1);
+                    }
+                    else if (currentStone.ToString().Length % 2 == 0)
+                    {
+                        int numberOfDigits = currentStone.ToString().Length;
+                        int halfDigit = numberOfDigits / 2;
+                        BigInteger divisor = BigInteger.Pow(10, halfDigit);
+                        BigInteger leftStone = currentStone / divisor;
+                        BigInteger rightStone = currentStone % divisor;
+
+                        newStones.Add(leftStone);
+                        newStones.Add(rightStone);
+                    }
+                    else
+                    {
+                        newStones.Add(currentStone * 2024);
+                    }
+                }
+
+                currentStones = newStones; // Replace old stones with the new list for the next blink
+            }
+
+            totalStones += currentStones.Count; // Add the count of stones after all blinks for this stone
+        }
+
+        Console.WriteLine($"Total Stones: {totalStones}");
     }
 
     public BigInteger GetNumberOfStones()
