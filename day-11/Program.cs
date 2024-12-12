b13230c71d1b70711a68ab0a10a9d0be4c16739b
itsOwlbit <51840045+itsOwlbit@users.noreply.github.com>
2024-12-11 18:30:43 -0800

    Get input data from file.

--- day-11/Program.cs
+++ day-11/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string filepath = "day-11/aoc-day11-test.txt";
+
+StonesLine stoneLine = new StonesLine(filepath);
+
+stoneLine.DisplayStones();
+Console.WriteLine();
+
+class StonesLine
+{
+    List<int> _stones;
+
+    public StonesLine(string filepath)
+    {
+        _stones = new List<int>();
+
+        this.GetStonesFromFile(filepath);
+    }
+
+    private void GetStonesFromFile(string filepath)
+    {
+        using (StreamReader sr = new StreamReader(filepath))
+        {
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                var parts = line.Split(" ");
+
+                foreach (var part in parts)
+                {
+                    if (int.TryParse(part, out int number))
+                    {
+                        _stones.Add(number);
+                    }
+                }
+            }
+        }
+    }
+
+    public void DisplayStones()
+    {
+        Console.WriteLine($"Stone line: {string.Join(", ", _stones)}");
+    }
+}
