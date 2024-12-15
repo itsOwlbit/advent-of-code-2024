cc823694c7262fb5e3c93997e1f8601d09f69efa
itsOwlbit <51840045+itsOwlbit@users.noreply.github.com>
2024-12-15 14:37:58 -0800

    wip day 14 part 1

--- day-14/Program.cs
+++ day-14/Program.cs
@@ -1,27 +1,35 @@
 ﻿using System.Text.RegularExpressions;
 
 string filepath = "day-14/aoc-day14-test.txt";
-int width = 101;
-int height = 103;
+int row = 103;
+int col = 101;
 
-RobotGrid robotGrid = new RobotGrid(filepath, width, height);
+RobotGrid robotGrid = new RobotGrid(filepath, row, col);
 robotGrid.PrintRobotData();
 Console.WriteLine();
+
+robotGrid.DisplayRobotLocationGraph();
+Console.WriteLine();
+
+int secondsPassed = 1;
 
 class RobotGrid
 {
     List<(int X, int Y)> _robotPositions;
     List<(int XVelocity, int YVelocity)> _robotVelocities;
-    int _gridWidth;
-    int _gridHeight;
+    List<List<char>> _robotLocationGraph;
+    int _gridRow;
+    int _gridCol;
 
-    public RobotGrid(string filepath, int width, int height)
+    public RobotGrid(string filepath, int row, int col)
     {
         _robotPositions = new List<(int X, int Y)>();
         _robotVelocities = new List<(int XVelocity, int YVelocity)>();
-        _gridWidth = width;
-        _gridHeight = height;
+        _robotLocationGraph = new List<List<char>>();
+        _gridRow = row;
+        _gridCol = col;
 
+        CreateRobotLocationGraph();
         GetRobotDataFromFile(filepath);
     }
 
@@ -50,6 +58,55 @@ class RobotGrid
                     _robotVelocities.Add((velX, velY));
                 }
             }
+        }
+    }
+
+    private void CreateRobotLocationGraph()
+    {
+        _robotLocationGraph = new List<List<char>>();
+
+        for (int r = 0; r < _gridRow; r++)
+        {
+            List<char> row = new List<char>();
+
+            for (int c = 0; c < _gridCol; c++)
+            {
+                row.Add('.');
+            }
+
+            _robotLocationGraph.Add(row);
+        }
+    }
+
+    private void UpdateRobotLocationGraph()
+    {
+        // Reset the graph with '.' for empty spaces
+        CreateRobotLocationGraph();
+
+        // Update the graph with robot positions
+        foreach (var (x, y) in _robotPositions)
+        {
+            if (_robotLocationGraph[y][x] == '.')
+            {
+                // If the cell is empty, set it to '1'
+                _robotLocationGraph[y][x] = '1';
+            }
+            else
+            {
+                // If the cell already contains a number, increment it
+                int currentValue = _robotLocationGraph[y][x] - '0';
+                _robotLocationGraph[y][x] = (char)(currentValue + 1 + '0');
+            }
+        }
+    }
+
+    public void DisplayRobotLocationGraph()
+    {
+        UpdateRobotLocationGraph();
+
+        foreach (var row in _robotLocationGraph)
+        {
+            Console.WriteLine(string.Join("", row));
         }
     }
 
